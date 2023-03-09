35d9973eb3d08d040d82a65464920b605db98674
helgihu <schmid@cis.lmu.de>
2023-03-09 14:29:24 +0100

    The tokenizer has been improved.

--- cmd/split-apostrophies.py
+++ cmd/split-apostrophies.py
@@ -5,91 +5,132 @@ from collections import defaultdict, Counter
 import fileinput
 
 # replacements operations for '
-op_prob = {op: 0.125 for op in ("e","e "," e"," ei"," eine"," de"," da","ei")}
-op_prob["'"] = 0.00001  # preference for replacing '
-
-# input of the data
+apo_prob = {" da":1, " eine":1,
+            " e":0.01, " ei":0.01, " de":0.01, "e ":0.01, "e":0.01, "ei":0.01, "e e":0.01,
+            "'":0.0001}
+# input of the data (1 token per line)
 all_tokens = [line.rstrip() for line in fileinput.input()]
+
+
+### tokenizer training    
 
 # non-empty tokens    
 tokens = [token.lower() for token in all_tokens if token]
+pair_freq = Counter(zip([''] + tokens, tokens + ['']))
 
-# estimation of word and word pair probabilities
-freq = Counter(tokens)
-N = sum(freq.values())
-prob = defaultdict(float, {x: f/N for x, f in freq.items()})
+def estimate_prob(freq):
+    total = sum(freq.values())
+    prob = {elem: f/total for elem, f in freq.items()}
+    return defaultdict(float, prob)
 
-# estimation of conditional probabilities using Kneser-Ney smoothing
-freq2 = Counter(zip(tokens[:-1], tokens[1:]))
-N1 = sum(1 for f in freq2.values() if f == 1)
-N2 = sum(1 for f in freq2.values() if f == 2)
-discount = N1 / (N1 + 2 * N2)
-cond_prob = {(x,y): (f - discount) / freq[x] for (x,y), f in freq2.items()}
-cond_prob = defaultdict(float, cond_prob)
-backoff = defaultdict(lambda: 1.0)
-for (x,y), p in cond_prob.items():
-    backoff[x] -= p
-freq1 = defaultdict(int)
-for (x,y) in freq2:
-    freq1[y] += 1
-total = sum(freq1.values())
-backoff_prob = defaultdict(float, {y: f/total for y, f in freq1.items()})
+def compute_discount(freq):
+    N1, N2 = 0, 0
+    for f in freq.values():
+        if f <= 1:
+            N1 += 1
+        elif f <= 2:
+            N2 += 1
+    return N1 / (N1 + 2 * N2)
+
+def estimate_probs(pair_freq, apo_freq=None):
+
+    # prior word probabilities
+    word_freq = defaultdict(int)
+    for (_, word), f in pair_freq.items():
+        word_freq[word] += f
+    
+    # discounting
+    discount = compute_discount(word_freq)
+    discounted_freq = {word: max(0.01, f-discount) for word, f in word_freq.items()}
+    word_prob = estimate_prob(discounted_freq)
+
+    # estimation of conditional probabilities using Kneser-Ney smoothing
+    context_freq = defaultdict(int)
+    for (word, _), f in pair_freq.items():
+        context_freq[word] += f
+    discount = compute_discount(pair_freq)
+    cond_prob = defaultdict(float)
+    for (x,y), f in pair_freq.items():
+        if f > discount:
+            cond_prob[x,y] = (f - discount) / context_freq[x]
+    
+    # computation of backoff factors
+    backoff = defaultdict(lambda: 1.0)
+    for (x,y), p in cond_prob.items():
+        backoff[x] -= p
+
+    # estimation of backoff probabilities
+    backoff_freq = defaultdict(int)
+    for (x,y) in pair_freq:
+        backoff_freq[y] += 1  # Kneser-Ney smoothing
+    backoff_prob = estimate_prob(backoff_freq)
+
+    if apo_freq is None:
+        return word_prob, cond_prob, backoff, backoff_prob
+
+    # computation of the replacement probabilities
+    for seq in apo_freq:
+        freq = sum(apo_freq[seq].values())  # expected frequency of x ' y
+        total = 0.0                         # expected frequency of x seq y and x ' y
+        for elem, f in apo_freq[seq].items():
+            if ' ' in seq:
+                elem1, elem2 = elem.split(' ')
+                total += pair_freq[elem1, elem2]
+            else:
+                total += word_freq[elem]
+        apo_prob[seq] = freq / total
+    return word_prob, cond_prob, backoff, backoff_prob, apo_prob
+
+
+# initial M step
+word_prob, cond_prob, backoff, backoff_prob = estimate_probs(pair_freq)
 
 # computation of the probability of a replacement operation
 def get_prob(elem):
     elem = elem.lower()
-    if elem in prob: # always true after the first EM iteration
-        return prob[elem]
+    if elem in word_prob: # always true after the first EM iteration
+        return word_prob[elem]
     if ' ' not in elem:
         return 0.0
     e1, e2 = elem.split(" ")
-    p = prob[e1]
-    p *= cond_prob[e1, e2] + backoff[e1] * backoff_prob[e2]
+    p = word_prob[e1] * (cond_prob[e1, e2] + backoff[e1] * backoff_prob[e2])
     return p
 
-def increment_freq(elem, f):
-    freq[elem.lower()] += p
-
-prev = ''  # previous token for counting word pair frequencies
-
 for _ in range(2): # EM iterations
-    
+
     # E-step: computation of expected frequencies
     # for words, word pairs and replacement operations
-    freq = defaultdict(float)
-    op_freq = defaultdict(float)
+    apo_freq = {seq: defaultdict(float) for seq in apo_prob}
+    pair_freq = defaultdict(float)
+    prev = ''  # previous token for counting word pair frequencies
     for token in tokens:
         elems = token.split("'")
         if len(elems) == 1 or elems[0] == "" or elems[-1] == "":
-            increment_freq(token, 1)
-            increment_freq(f"{prev} {token}", p)
+            pair_freq[prev, token] += 1
         else:
+            # compute the posterior probability of each replacement operation
             elem1 = "'".join(elems[:-1])
             elem2 = elems[-1]
-            option_prob = {op: get_prob(elem1 + op + elem2) * op_p
-                           for op, op_p in op_prob.items()}
+            option_prob = {seq: get_prob(elem1 + seq + elem2) * apo_p
+                           for seq, apo_p in apo_prob.items()}
             total = sum(option_prob.values())
-            for op, p in option_prob.items():
-                elem = elem1 + op + elem2
-                p /= total # normalize the probabilities to sum to 1
-                op_freq[op] += p
+            option_prob = {seq: p / total for seq, p in option_prob.items()}
+            for seq, p in option_prob.items():
+                elem = elem1 + seq + elem2
+                apo_freq[seq][elem] += p
                 parts = elem.split(" ")
                 if len(parts) == 1:
-                    increment_freq(elem, p)
-                    increment_freq(f"{prev} {elem}", p)
+                    pair_freq[prev, elem] += p
                 else:
                     e1, e2 = parts
-                    increment_freq(e1, p)
-                    increment_freq(e2, p)
-                    increment_freq(f"{e1} {e2}", p)
-                    increment_freq(f"{prev} {e1}", p)
-                prev = parts[-1]
-    # M-step
-    total = sum(op_freq.values())
-    op_prob = {op: f/total for op, f in op_freq.items()}
-    prob = {x: f/N for x, f in freq.items()}
+                    pair_freq[prev, e1] += p
+                    pair_freq[e1, e2] += p
+        prev = token  # problematic if previous token contains an apostrophe
 
-# tokenization    
+    # M Step
+    word_prob, cond_prob, backoff, backoff_prob, apo_prob = estimate_probs(pair_freq, apo_freq)
+    
+### tokenization
 for token in all_tokens:
     elems = token.split("'")
     if len(elems) == 1 or elems[0] == "" or elems[-1] == "":
@@ -97,13 +138,15 @@ for token in all_tokens:
     else:
         elem1 = "'".join(elems[:-1])
         elem2 = elems[-1]
-        option_prob = {op: get_prob(elem1 + op + elem2) * op_p
-                       for op, op_p in op_prob.items()}
-        best_option = max(option_prob.keys(), key=option_prob.get)
-        if best_option[0] == ' ':
+        option_prob = {seq: get_prob(elem1 + seq + elem2) * apo_p
+                       for seq, apo_p in apo_prob.items()}
+        best_seq = max(option_prob.keys(), key=option_prob.get)
+        if best_seq[0] == ' ':
             print(f"{elem1}\n'{elem2}")
-        elif best_option[-1] == ' ':
+        elif best_seq[-1] == ' ':
             print(f"{elem1}'\n{elem2}")
+        elif ' ' in best_seq:
+            print(f"{elem1}'\n'{elem2}")
         else:
             print(token)
         
--- cmd/tokenize-songs.sh
+++ cmd/tokenize-songs.sh
@@ -2,7 +2,7 @@
 
 cmd/tokenize.pl $*  |
     perl -pe 's/^$/<>/' |
-    cmd/disamb-period-utf8.pl -a -f lib/german-tokenizer |
+    cmd/disamb-period-utf8.pl -f lib/german-tokenizer |
     perl -pe 's/^(.*[^0-9].*[0-9]+)\.$/$1\n\./' | 
     cmd/disamb-num-period-utf8.pl -f lib/german-tokenizer.num |
     cmd/split-apostrophies.py |
